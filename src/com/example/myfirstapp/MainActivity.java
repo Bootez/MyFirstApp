a8bd18343a2f05348a3c8174f86d8627c6affc51
Bootez <8ootez@gmail.com>
2014-04-18 09:45:03 +0800

    Respond to the Send Button

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.myfirstapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -11,7 +12,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
